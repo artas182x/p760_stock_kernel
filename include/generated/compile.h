c2e41dd36c0731d1182f9425a136ce46f6a048c4
artas182x <artas182x@gmail.com>
2013-09-07 20:51:06 +0200

    Revert "Eliminado configuraciones inútiles y añadido la única que se usa"
    This reverts commit 54d9a3b9ae30b6f6f2fffa498ba116ed343e1c41.

--- include/generated/compile.h
+++
@@ -1,7 +0,0 @@
-/* This file is auto generated, version 0 */
-/* SMP PREEMPT */
-#define UTS_MACHINE "arm"
-#define UTS_VERSION "#0 SMP PREEMPT Sat Sep 7 18:42:16 CEST 2013"
-#define LINUX_COMPILE_BY "artur"
-#define LINUX_COMPILE_HOST "artur-EP45-DS3L"
-#define LINUX_COMPILER "gcc version 4.4.3 (GCC) "
