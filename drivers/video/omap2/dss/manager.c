b69eb632b486ffa91afc61914483c74fafda7e7d
artas182x <artas182x@gmail.com>
2013-09-11 20:36:55 +0200

    Revert "Test gamma control"
    This reverts commit eb68ceb2a1f67244c906388763e74b1528bd206f.

--- drivers/video/omap2/dss/manager.c
+++ drivers/video/omap2/dss/manager.c
@@ -384,38 +384,6 @@ static ssize_t manager_cpr_coef_store(struct omap_overlay_manager *mgr,
 	return size;
 }
 
-static ssize_t manager_gamma_show(
-    struct omap_overlay_manager *mgr, char *buf)
-{
-  return snprintf(buf, PAGE_SIZE, "%d\n", mgr->info.gamma);
-}
-
-static ssize_t manager_gamma_store(
-    struct omap_overlay_manager *mgr,
-    const char *buf, size_t size)
-{
-  struct omap_overlay_manager_info info;
-  int gamma_value;
-  int r;
-
-  if (sscanf(buf, "%d", &gamma_value) != 1)
-    return -EINVAL;
-
-  mgr->get_manager_info(mgr, &info);
-
-  info.gamma = gamma_value;
-
-  r = mgr->set_manager_info(mgr, &info);
-  if (r)
-    return r;
-
-  r = mgr->apply(mgr);
-  if (r)
-    return r;
-
-  return size;
-}
-
 #ifdef CONFIG_LGE_BROADCAST_TDMB
 static ssize_t manager_dmb_coefs_show(struct omap_overlay_manager *mgr, char *buf)
 {
@@ -549,10 +517,6 @@ static MANAGER_ATTR(cpr_coef, S_IRUGO|S_IWUSR,
 		manager_cpr_coef_show,
 		manager_cpr_coef_store);
 
-static MANAGER_ATTR(gamma, S_IRUGO|S_IWUSR,
-      manager_gamma_show,
-      manager_gamma_store);
-
 #ifdef CONFIG_LGE_BROADCAST_TDMB
 static MANAGER_ATTR(dmb_coefs, S_IRUGO | S_IWUSR, 
 		manager_dmb_coefs_show, 
@@ -569,7 +533,6 @@ static struct attribute *manager_sysfs_attrs[] = {
 	&manager_attr_alpha_blending_enabled.attr,
 	&manager_attr_cpr_enable.attr,
 	&manager_attr_cpr_coef.attr,
-	&manager_attr_gamma.attr,
 
 #ifdef CONFIG_LGE_BROADCAST_TDMB	
 	&manager_attr_dmb_coefs.attr,
@@ -718,8 +681,6 @@ struct manager_cache_data {
 
 	enum omap_dss_trans_key_type trans_key_type;
 	u32 trans_key;
-	u8 gamma;
-
 	bool trans_enabled;
 
 	bool alpha_enabled;
@@ -1339,7 +1300,6 @@ static void configure_manager(enum omap_channel channel)
 	dispc_set_default_color(channel, c->default_color);
 	dispc_set_trans_key(channel, c->trans_key_type, c->trans_key);
 	dispc_enable_trans_key(channel, c->trans_enabled);
-	dispc_enable_gamma(channel, c->gamma);
 
 	/* if we have OMAP3 alpha compatibility, alpha blending is always on */
 	if (dss_has_feature(FEAT_ALPHA_OMAP3_COMPAT)) {
@@ -2246,7 +2206,6 @@ static int omap_dss_mgr_apply(struct omap_overlay_manager *mgr)
 	mc->alpha_enabled = mgr->info.alpha_enabled;
 	mc->cpr_coefs = mgr->info.cpr_coefs;
 	mc->cpr_enable = mgr->info.cpr_enable;
-	mc->gamma = mgr->info.gamma;
 
 #ifdef CONFIG_LGE_BROADCAST_TDMB
 	mc->dmb_coefs.direction = mgr->info.dmb_coefs.direction;
