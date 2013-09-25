f8239aab660d8c234f427421fda2d8fb6e508014
artas182x <artas182x@gmail.com>
2013-09-25 19:21:57 +0200

    Decreased max freq

--- arch/arm/mach-omap2/omap2plus-cpufreq.c
+++ arch/arm/mach-omap2/omap2plus-cpufreq.c
@@ -513,7 +513,7 @@ static int __cpuinit omap_cpu_init(struct cpufreq_policy *policy)
 	cpufreq_frequency_table_get_attr(freq_table, policy->cpu);
 
 	policy->min = 230400;
-	policy->max = 1036800;
+	policy->max = 921600;
 	policy->cur = omap_getspeed(policy->cpu);
 
 	for (i = 0; freq_table[i].frequency != CPUFREQ_TABLE_END; i++)
