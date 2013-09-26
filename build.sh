b9aec2cb41d6671670d134f83cd43edd1170a52b
artas182x <artas182x@gmail.com>
2013-09-26 20:49:30 +0200

    GPU sysfs oc, some small changes
    Signed-off-by: artas182x <artas182x@gmail.com>

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "Cleaning after last build"
-make mrproper
+make clean
 echo "Preparing for build"
 make u2_p760_defconfig ARCH=arm
 echo "Starting building.."
-make -j6 ARCH=arm CROSS_COMPILE=/home/artur/android-ndk-r9/toolchains/arm-linux-androideabi-4.7/prebuilt/linux-x86_64/bin/arm-linux-androideabi- zImage
+make -j5 ARCH=arm CROSS_COMPILE=../prebuilt/linux-x86/toolchain/arm-eabi-4.4.3/bin/arm-eabi- zImage
